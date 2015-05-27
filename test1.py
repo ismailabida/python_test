a5c7bdb58b93ae3b2b15e301003902973c03f66c
ismail <ismail.abida.60@gmail.com>
2015-05-27 14:58:54 +0200

    first commit

--- test1.py
+++ test1.py
@@ -6,7 +6,7 @@ class TestStringMethods(unittest.TestCase):
       self.assertEqual('ismail'.upper(), 'ISMAIL')
 
   def test_isupper(self):
-      self.assertTrue('ismail'.isupper())
+      self.assertTrue('ISMAIL'.isupper())
       self.assertFalse('ismail'.isupper())
 
   def test_split(self):
